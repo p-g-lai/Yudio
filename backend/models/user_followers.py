76a8398b3b866a2875798a2bc8180fcb0e9d69e8
Phuc <pglai.us@gmail.com>
2024-07-29 20:45:43 -0400

    env setup

--- backend/models/user_followers.py
+++
@@ -1,6 +0,0 @@
-from app import db
-
-user_followers_join = db.Table('user_followers',
-  db.Column('follower_id', db.Integer, db.ForeignKey('users.id')),
-  db.Column('followed_id', db.Integer, db.ForeignKey('users.id'))
-)
--- backend/models/user_genre.py
+++
@@ -1,6 +0,0 @@
-from app import db
-
-user_genre_join = db.Table('user_genre',
-  db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
-  db.Column('genre_id', db.Integer, db.ForeignKey('genres.id'), primary_key=True)
-)
